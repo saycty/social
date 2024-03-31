232ec04a85ac0e50714e0bbe05ab65a13f823f7a
sayan chakrabortty <sayanchakrabortty3@gmail.com>
2024-03-31 22:41:28 +0530

    topbar update

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { TbSocial } from "react-icons/tb";
+import { Link } from "react-router-dom";
+import TextInput from "./TextInput";
+import CustomButton from "./CustomButton";
+import { useForm } from "react-hook-form";
+import { BsMoon, BsSunFill } from "react-icons/bs";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import {setTheme} from "../redux/theme";
+import {Logout} from "../redux/userSlice";
 const TopBar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { theme } = useSelector((state) => state.theme);
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handelSubmit,
+    formState: { errors },
+  } = useForm();
 
-export default TopBar
+  const handleTheme = () => {
+    const themeValue = theme === "light" ? "dark" : "light";
+    dispatch(setTheme(themeValue));
+  };
+
+  const handleSearch = async (data) => {};
+
+  return (
+    <div
+      className="topbar w-full flex items-center justify-between
+    py-3 md:py-6 px-4 bg-primary"
+    >
+      <Link to="/" className="flex gap-2 items-center">
+        <div className="p-1 md:p-2 bg-[#065ad8] rounded text-white">
+          <TbSocial />
+        </div>
+        <span className="text-xl md:text-2xl text-[#065ad8] font-semibold">
+          ShareFun
+        </span>
+      </Link>
+
+      <form
+        className="hidden md:flex items-center justify-center"
+        onSubmit={handelSubmit(handleSearch)}
+      >
+        <TextInput
+          placeholder="search..."
+          styles="w=[18rem] lg:w-[38rem] rounded-1-full py-3"
+          register={register("search")}
+        />
+        <CustomButton
+          title="Search"
+          type="submit"
+          containerStyles="bg-[#0444a4] text-white px-6 py-2.5 mt-2 rounded-r-full"
+        />
+      </form>
+
+      {/*Icons */}
+      <div className="flex gap-4 items-center text-ascent-1 text-md md:text-xl">
+        <button onClick={() => handleTheme()}>
+          {theme ? <BsMoon /> : <BsSunFill />}
+        </button>
+        <div className="hidden lg:flex">
+          <IoMdNotificationsOutline />
+        </div>
+        <div>
+            <CustomButton
+            onClick={()=>dispatch(Logout())}
+            title='Log Out'
+            containerStyles='text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2
+            border border-[#666] rounded-full'/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import TopBar from '../components/TopBar';
 
 const Home = () => {
-  const{user}=useSelector(state=>state.users)
+  const{user}=useSelector((state)=>state.user)
 
 
   return (
     <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor 
     lg:rounded-lg h-screen overflow-hidden'>
       <TopBar/>
+
+      <div className="w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full">
+        {/* left */}
+        <div className='hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6 overflow-y-auto'>
+        </div>
+
+        
+
+
+        {/* right */}
+        <div></div>
+        {/* center */}
+        <div></div>
+      </div>
     </div>
   )
 }
 
 export default Home
+
+
